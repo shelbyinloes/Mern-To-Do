9b10f6b82480e355f917160b2abf3b1c496fb25f
Shelby Inloes <shelbyinloes@gmail.com>
2019-01-10 15:12:19 -0600

    finished with tutorial

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ router.get("/getData", (req, res) => {
 // this method overwrites existing data in our database
 router.post("/updateData", (req, res) => {
   const { id, update } = req.body;
-  Data.findOneAndUpdate(id, update, err => {
+  Data.findByIdAndUpdate(id, update, err => {
     if (err) return res.json({ success: false, error: err });
     return res.json({ success: true });
   });
@@ -53,7 +53,7 @@ router.post("/updateData", (req, res) => {
 // this method removes existing data in our database
 router.delete("/deleteData", (req, res) => {
   const { id } = req.body;
-  Data.findOneAndDelete(id, err => {
+  Data.findByIdAndDelete(id, err => {
     if (err) return res.send(err);
     return res.json({ success: true });
   });
